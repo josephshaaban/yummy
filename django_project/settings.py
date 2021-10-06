2936e690c10ca6c842c2def51a7628befb0c167a
Youssef Shaaban <y.shaaban@lit-co.net>
2021-10-07 01:00:14 +0300

    fix styling for select2

--- django_project/settings.py
+++ django_project/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = 'exhlfdat&vfum(-34*c2uroi(($ww(yo$9pv98=e6p^gl(-eoj'
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -80,7 +78,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'django_project.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
@@ -90,7 +87,6 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
@@ -110,7 +106,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
@@ -123,7 +118,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
@@ -141,7 +135,6 @@ LOGIN_URL = 'login'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
 
-
 CRISPY_CLASS_CONVERTERS = {
     'textinput': "form-control cst__radius",
     'textarea': "form-control md-textarea rounded-1",
@@ -150,9 +143,8 @@ CRISPY_CLASS_CONVERTERS = {
 }
 
 # change select2 settings to local versions
-SELECT2_JS = "/blog/jquery_select2/select2-4.0.10-dist/js/select2.min.js"
-SELECT2_CSS = "/blog/jquery_select2/select2-4.0.10-dist/css/select2.min.css"
+SELECT2_JS = "/blog/jquery_select2/select2-4.0.10-dist/js/select2.js"
+SELECT2_CSS = "/blog/jquery_select2/select2-4.0.10-dist/css/select2.css"
 SELECT2_I18N_PATH = '/blog/jquery_select2/select2-4.0.10-dist/js/i18n'
 
 # USE_DJANGO_JQUERY = True
-
